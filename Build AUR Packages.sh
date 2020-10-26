34f9409a0d8f6aaa5e304d1faeee35bf997cf48e
kDylanHayes <57426223+kDylanHayes@users.noreply.github.com>
2020-10-25 20:01:25 -0400

    Fixed spacing and added comments (header)

--- Build AUR Packages.sh
+++ Build AUR Packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #Building packages I like (Dylan)
+#Version 1.0 
+#Created on 2020-10-25 
 
 num_programs=2
 
@@ -10,7 +12,7 @@ cd tmp
 echo install up to $num_programs AUR programs, exit by pressing Ctrl+C 
 
 for i in {1..$num_programs}
-	do	  
+do	  
 	read -p 'please enter a valid AUR package name: ' program1
 	git clone https://aur.archlinux.org/$program1.git
 	cd $program1
@@ -24,6 +26,7 @@ for i in {1..$num_programs}
 	fi
 
 done 
+#clean up
 cd ~
 rm -rf tmp 
 
